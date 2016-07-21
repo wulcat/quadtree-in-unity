2ae1071a24135796e1bad657fca618b215ba1d27
Zoe Love <zoelove.zle@gmail.com>
2016-07-22 00:56:08 +0530

    Added quadTree and map scripts

---
+++ Assets/Map/Map.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Map : MonoBehaviour {
+	public BoxCollider main ;
+	public QuadTree quadTree = new QuadTree(
+		new Rect(0,0,1000,1000),
+		4,
+		4,
+		4
+	) ;
+	private List<GameObject> tar = new List<GameObject>() ;
+	public BoxCollider[] quadReturns ;
+	// Use this for initialization
+	void Start () {
+		// quadTree.Move(transform.position);
+		quadTree.clear() ;
+		quadTree.split() ;
+		tar.AddRange(GameObject.FindGameObjectsWithTag("tar")) ;
+		for(var i = 0 ; i < tar.Count ; i++) {
+			quadTree.insert(tar[i].GetComponent<BoxCollider>()) ;
+		}
+		// transform.localScale = new Vector3(quadTree.mapSize.max.x , 0 , quadTree.mapSize.max.y);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		quadReturns = null ;
+		quadTree.clear() ;
+		quadTree.split() ;
+		for(var i = 0 ; i < tar.Count ; i++) {
+			quadTree.insert(tar[i].GetComponent<BoxCollider>()) ;
+		}
+		quadReturns = quadTree.retrieve(main);
+
+	}
+	void OnDrawGizmos() {
+		var bounds = quadTree.mapSize ;
+		Gizmos.color = Color.red ;
+		Gizmos.DrawWireCube(transform.position , new Vector3(bounds.max.x , 0 , bounds.max.y));
+		for(var i = 0 ; i < quadTree.Nodes.Count ; i++) {
+			bounds = quadTree.Nodes[i].mapSize ;
+			Gizmos.color = Color.green ;
+			Gizmos.DrawWireCube(bounds.center , new Vector3(bounds.max.x , 0 , bounds.max.y));
+		}
+	}
+}
